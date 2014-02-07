70630a1bedf946d089df5fbf68edbe02b7f79b6b
U-portaljr\turkycat <turkycat@portaljr.(none)>
2014-02-06 19:03:06 -0600

    added the all important exclamation point to illustrate giT

--- src/mdc/collab/nightreader/MainActivity.java
+++ src/mdc/collab/nightreader/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
 		
-		//this is where we will resume next time yay!
+		//this is where we will resume next time yay!!
 	}
 
 	@Override
