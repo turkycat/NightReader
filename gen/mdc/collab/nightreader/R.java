7871f8a07b8e689f456de4fe63dd88b6fe9c4375
Jesse Frush <jesse.frush@gmail.com>
2014-03-10 01:55:24 -0500

    Added eject image, changed application base theme

--- gen/mdc/collab/nightreader/R.java
+++ gen/mdc/collab/nightreader/R.java
@@ -21,12 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int eject=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int AudioView=0x7f080000;
         public static final int LoadButton=0x7f080002;
-        public static final int action_settings=0x7f080003;
+        public static final int MainActivity_MenuText=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int filler=0x7f080001;
     }
     public static final class layout {
@@ -39,6 +41,7 @@ public final class R {
         public static final int main=0x7f070002;
     }
     public static final class string {
+        public static final int MainActivity_MenuTextString=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int buttonText=0x7f050002;
